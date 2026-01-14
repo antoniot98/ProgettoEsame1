f1a5bca37a202fbec225da6ef85113435798acd6
Antonio <tessitoreantonio098@gmail.com>
2026-01-14 14:43:21 +0100

    Da sistemare

--- Funzioni.h
+++ Funzioni.h
@@ -2,9 +2,17 @@
 #define LIBRERIA_H
 
 
-void controllo_lettere(char * testo);
-void posizione_lettera(char chiave, char *mat, int n_righe, int n_col, int *riga, int*col);
-void elimina_spazi(char*, char*);
+void controllo_lettere(char *testo);
+
+void posizione_lettera(char chiave, char *mat, int n_righe, int n_col, int *riga, int *col);
+
+void elimina_spazi(char *, char *);
+
 void Crittografia(char *testo, char *mat_sostituzione, int n_righe, int n_col, char *testo_crittografato);
 
-#endif 
+void ripristina_lettere(char *testo);
+
+void stampa_matrice(char *mat, int n_righe, int n_col);
+
+void Decrittografia(char *testo_crittografato, char *mat_sostituzione, int n_righe, int n_col, char *testo);
+#endif
--- main.c
+++ main.c
@@ -6,19 +6,32 @@ int main(int argc, char const *argv[]) {
     char testo_crittografato[100] = "";
     // char messaggio[100]="LET US MEET AT NOON";
     char messaggio[100] = "";
+    char testo_decrittografato[100] = "";
     char mat_sostituzione[][8] = {
         {'8', 'J', 'E', 'Q', 'D', 'N', '5', 'O'},
         {'P', 'U', '3', 'A', 'R', 'F', 'L', 'W'},
         {'4', 'V', 'C', '2', 'T', 'M', 'B', 'I'},
         {'K', '7', 'Z', 'S', 'G', 'X', 'H', 'Y'}
     };
+    srand(time(NULL));
 
-    // stampa_a2d_char(&mat_sostituzione[0][0],4,8);
 
+    for (int i = 1; i < 4 * 8; i++) {
+        char temp;
+        int riga1 = rand() % 4;
+        int riga2 = rand() % 4;
+        int colonna1 = rand() % 8;
+        int colonna2 = rand() % 8;
+        temp = mat_sostituzione[riga1][colonna1];
+        mat_sostituzione[riga1][colonna1] = mat_sostituzione[riga2][colonna2];
+        mat_sostituzione[riga2][colonna2] = temp;
+    }
+    stampa_matrice(&mat_sostituzione[0][0], 4, 8);
     printf("Inserisci messaggio da crittografare:");
     gets(messaggio);
-    printf("\n%s\n", messaggio);
-    Crittografia(messaggio, &mat_sostituzione[0][0], 4, 8, testo_crittografato);
 
-    printf("\n%s\n", testo_crittografato);
+    Crittografia(messaggio, &mat_sostituzione[0][0], 4, 8, testo_crittografato);
+    Decrittografia(testo_crittografato, &mat_sostituzione[0][0], 4, 8, testo_decrittografato);
+    printf("\nIl testo crittografato e uguale a:%s\n", testo_crittografato);
+    printf("\nIl testo decrittografato e uguale a:%s\n", testo_decrittografato);
 }
