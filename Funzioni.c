23759220488045563be4017a2a7069bbb02365cd
Antonio <tessitoreantonio098@gmail.com>
2026-01-14 17:23:27 +0100

    Programma Funzionante

--- Funzioni.c
+++ Funzioni.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
-void stampa_matrice(char *mat, int n_righe,int n_col) {
-    for (int i=0;i<n_righe;i++) {
-        for (int j=0;j<n_col;j++) {
-            printf("%5c",*(mat + i*n_col + j));
+
+void stampa_matrice(char *mat, int n_righe, int n_col) {
+    for (int i = 0; i < n_righe; i++) {
+        for (int j = 0; j < n_col; j++) {
+            printf("%5c", *(mat + i * n_col + j));
         }
         printf("\n");
     }
 }
+
 void posizione_lettera(char chiave, char *mat, int n_righe, int n_col, int *riga, int *colonna) {
-    int i = 0, trova = 0;
+    int i = 0;
 
     for (i = 0; i < n_righe; i++) {
         for (int j = 0; j < n_col; j++) {
@@ -54,41 +56,37 @@ void controllo_lettere(char *testo) {
         }
         i = i + 2;
     }
-    if (strlen(testo)%2!=0) {
-        *(testo+strlen(testo))='X';
+    if (strlen(testo) % 2 != 0) {
+        *(testo + strlen(testo)) = 'X';
     }
-    printf("Sono io\n%s\n", testo);
 }
+
 void ripristina_lettere(char *testo) {
     char temp[100];
     int i = 0;
-    printf("Testo da ripristinare \n%s\n", testo);
+
 
     while (i < (strlen(testo) - 1)) {
-        if (*(testo+i)=='X' && *(testo+i+1)==*(testo+i-1)) {
+        if (*(testo + i) == 'X' && *(testo + i + 1) == *(testo + i - 1)) {
             strcpy(temp, testo + i + 1);
-            strcpy(testo+i,temp);
+            strcpy(testo + i, temp);
         }
         i++;
     }
-    if (*(testo+strlen(testo)-1)=='X') {
-        *(testo+strlen(testo)-1)='\0';
+    if (*(testo + strlen(testo) - 1) == 'X') {
+        *(testo + strlen(testo) - 1) = '\0';
     }
-
-
-
-    //printf("\n%s\n", testo);
 }
 
 void Crittografia(char *testo, char *mat_sostituzione, int n_righe, int n_col, char *testo_crittografato) {
     int i = 0;
     int riga1, colonna1, riga2, colonna2;
     controllo_lettere(testo);
-    printf("%s\n",testo);
+
     while (i < strlen(testo) - 1) {
         posizione_lettera(*(testo + i), mat_sostituzione, n_righe, n_col, &riga1, &colonna1);
         posizione_lettera(*(testo + i + 1), mat_sostituzione, n_righe, n_col, &riga2, &colonna2);
-        printf("riga 1 %d, colonna 1 %d, riga2 %d, colonna2 %d\n", riga1, colonna1, riga2, colonna2);
+
         if (riga1 == riga2) {
             if (colonna1 < n_col - 1 && colonna2 < n_col - 1) {
                 *(testo_crittografato + i) = *(mat_sostituzione + riga1 * n_col + colonna1 + 1);
@@ -103,13 +101,13 @@ void Crittografia(char *testo, char *mat_sostituzione, int n_righe, int n_col, c
             i = i + 2;
         } else if (colonna1 == colonna2) {
             if (riga1 < n_righe - 1 && riga2 < n_righe - 1) {
-                *(testo_crittografato + i) = *(mat_sostituzione + riga1 + 1 * n_col + colonna1);
-                *(testo_crittografato + i + 1) = *(mat_sostituzione + riga2 + 1 * n_col + colonna2);
+                *(testo_crittografato + i) = *(mat_sostituzione + (riga1 + 1) * n_col + colonna1);
+                *(testo_crittografato + i + 1) = *(mat_sostituzione + (riga2 + 1) * n_col + colonna2);
             } else if (riga1 == n_righe - 1) {
                 *(testo_crittografato + i) = *(mat_sostituzione + 0 * n_col + colonna1);
-                *(testo_crittografato + i + 1) = *(mat_sostituzione + riga2 + 1 * n_col + colonna2);
+                *(testo_crittografato + i + 1) = *(mat_sostituzione + (riga2 + 1) * n_col + colonna2);
             } else {
-                *(testo_crittografato + i) = *(mat_sostituzione + riga1 + 1 * n_col + colonna1);
+                *(testo_crittografato + i) = *(mat_sostituzione + (riga1 + 1) * n_col + colonna1);
                 *(testo_crittografato + i + 1) = *(mat_sostituzione + 0 * n_col + colonna2);
             }
 
@@ -120,38 +118,38 @@ void Crittografia(char *testo, char *mat_sostituzione, int n_righe, int n_col, c
             i = i + 2;
         }
     }
-    printf("%s\n",testo_crittografato);
 }
-void Decrittografia (char *testo_crittografato, char *mat_sostituzione, int n_righe, int n_col, char *testo) {
+
+void Decrittografia(char *testo_crittografato, char *mat_sostituzione, int n_righe, int n_col, char *testo) {
     int i = 0;
     int riga1, colonna1, riga2, colonna2;
 
-    while (i <= strlen(testo_crittografato)-1) {
+    while (i <= strlen(testo_crittografato) - 1) {
         posizione_lettera(*(testo_crittografato + i), mat_sostituzione, n_righe, n_col, &riga1, &colonna1);
         posizione_lettera(*(testo_crittografato + i + 1), mat_sostituzione, n_righe, n_col, &riga2, &colonna2);
 
         if (riga1 == riga2) {
-            if (colonna1 > 0 && colonna2 >0) {
+            if (colonna1 > 0 && colonna2 > 0) {
                 *(testo + i) = *(mat_sostituzione + riga1 * n_col + colonna1 - 1);
                 *(testo + i + 1) = *(mat_sostituzione + riga2 * n_col + colonna2 - 1);
             } else if (colonna1 == 0) {
-                *(testo + i) = *(mat_sostituzione + riga1 * n_col + n_col -1);
-                *(testo+ i + 1) = *(mat_sostituzione + riga2 * n_col + colonna2 - 1);
+                *(testo + i) = *(mat_sostituzione + riga1 * n_col + n_col - 1);
+                *(testo + i + 1) = *(mat_sostituzione + riga2 * n_col + colonna2 - 1);
             } else {
-                *(testo + i + 1) = *(mat_sostituzione + riga2 * n_col + n_col -1);
+                *(testo + i + 1) = *(mat_sostituzione + riga2 * n_col + n_col - 1);
                 *(testo + i) = *(mat_sostituzione + riga1 * n_col + colonna1 - 1);
             }
             i = i + 2;
         } else if (colonna1 == colonna2) {
             if (riga1 > 0 && riga2 > 0) {
-                *(testo + i) = *(mat_sostituzione + riga1 - 1 * n_col + colonna1);
-                *(testo + i + 1) = *(mat_sostituzione + riga2 - 1 * n_col + colonna2);
-            } else if (riga1 == n_righe - 1) {
-                *(testo + i) = *(mat_sostituzione + n_righe-1 * n_col + colonna1);
-                *(testo + i + 1) = *(mat_sostituzione + riga2 - 1 * n_col + colonna2);
+                *(testo + i) = *(mat_sostituzione + (riga1 - 1) * n_col + colonna1);
+                *(testo + i + 1) = *(mat_sostituzione + (riga2 - 1) * n_col + colonna2);
+            } else if (riga1 == 0) {
+                *(testo + i) = *(mat_sostituzione + (n_righe - 1) * n_col + colonna1);
+                *(testo + i + 1) = *(mat_sostituzione + (riga2 - 1) * n_col + colonna2);
             } else {
-                *(testo + i) = *(mat_sostituzione + n_righe-1 * n_col + colonna1);
-                *(testo + i + 1) = *(mat_sostituzione + riga2 - 1 * n_col + colonna2);
+                *(testo + i) = *(mat_sostituzione + (n_righe - 1) * n_col + colonna1);
+                *(testo + i + 1) = *(mat_sostituzione + (riga2 - 1) * n_col + colonna2);
             }
 
             i = i + 2;
