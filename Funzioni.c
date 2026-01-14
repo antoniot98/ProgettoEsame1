34f528d8436d8fb4b74cacc6559fb65fd13a49ad
Antonio <tessitoreantonio098@gmail.com>
2026-01-14 10:39:41 +0100

    Final Crittografia with Input Text

--- Funzioni.c
+++ Funzioni.c
@@ -1,114 +1,89 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
-void posizione_lettera(char chiave, char *mat, int n_righe, int n_col, int *riga, int *colonna)
-{
 
+void posizione_lettera(char chiave, char *mat, int n_righe, int n_col, int *riga, int *colonna) {
     int i = 0, trova = 0;
 
-    for (i = 0; i < n_righe; i++)
-    {
-        for (int j = 0; j < n_col; j++)
-        {
-            if (*(mat + i * n_col + j) == chiave)
-            {
+    for (i = 0; i < n_righe; i++) {
+        for (int j = 0; j < n_col; j++) {
+            if (*(mat + i * n_col + j) == chiave) {
                 *riga = i;
                 *colonna = j;
             }
         }
     }
 }
-void elimina_spazi(char *testo)
-{
+
+void elimina_spazi(char *testo) {
     char testo_senza_spazi[100] = "\0";
     char *token;
     int i = 0;
     token = strtok(testo, " ");
 
-    while (token != NULL)
-    {
+    while (token != NULL) {
         strcat(testo_senza_spazi, token);
         token = strtok(NULL, " ");
     }
 
     strcpy(testo, testo_senza_spazi);
 }
-/* Funzione che controlla le coppie di lettere uguali ed aggiunge una X tra le due*/
-void controllo_lettere(char *testo)
-{
 
+/* Funzione che controlla le coppie di lettere uguali ed aggiunge una X tra le due*/
+void controllo_lettere(char *testo) {
     char temp[100];
     int i = 0;
     elimina_spazi(testo);
+    strupr(testo);
     printf("\n%s\n", testo);
+
     /*Ciclo while finche c'è il testo controlla le coppie di lettere se sono uguali copia la parte dopo in
     una variabile temporanea, aggiugne una x e copia il contenuto della variabile temporanea nell'array principale*/
-    while (i < (strlen(testo) - 1))
-    {
-
-        if (*(testo + i) == *(testo + i + 1))
-        {
+    while (i < (strlen(testo) - 1)) {
+        if (*(testo + i) == *(testo + i + 1)) {
             strcpy(temp, testo + i + 1);
             *(testo + i + 1) = 'X';
             strcpy(testo + i + 2, temp);
         }
         i = i + 2;
     }
+    printf("\n%s\n", testo);
 }
 
-void Crittografia(char *testo, char *mat_sostituzione, int n_righe, int n_col, char *testo_crittografato)
-{
-
+void Crittografia(char *testo, char *mat_sostituzione, int n_righe, int n_col, char *testo_crittografato) {
     int i = 0;
     int riga1, colonna1, riga2, colonna2;
-
-    while (i < strlen(testo) - 1)
-    {
+    controllo_lettere(testo);
+    while (i < strlen(testo) - 1) {
         posizione_lettera(*(testo + i), mat_sostituzione, n_righe, n_col, &riga1, &colonna1);
         posizione_lettera(*(testo + i + 1), mat_sostituzione, n_righe, n_col, &riga2, &colonna2);
 
-        if (riga1 == riga2)
-        {
-            if (colonna1 < n_col - 1 && colonna2 < n_col - 1)
-            {
+        if (riga1 == riga2) {
+            if (colonna1 < n_col - 1 && colonna2 < n_col - 1) {
                 *(testo_crittografato + i) = *(mat_sostituzione + riga1 * n_col + colonna1 + 1);
                 *(testo_crittografato + i + 1) = *(mat_sostituzione + riga2 * n_col + colonna2 + 1);
-            }
-            else if (colonna1 == n_col - 1)
-            {
+            } else if (colonna1 == n_col - 1) {
                 *(testo_crittografato + i) = *(mat_sostituzione + riga1 * n_col + 0);
                 *(testo_crittografato + i + 1) = *(mat_sostituzione + riga2 * n_col + colonna2 + 1);
-            }
-            else
-            {
+            } else {
                 *(testo_crittografato + i + 1) = *(mat_sostituzione + riga2 * n_col + 0);
                 *(testo_crittografato + i) = *(mat_sostituzione + riga1 * n_col + colonna1 + 1);
             }
             i = i + 2;
-        }
-        else if (colonna1 == colonna2)
-        {
-            if (riga1 < n_righe - 1 && riga2 < n_righe - 1)
-            {
+        } else if (colonna1 == colonna2) {
+            if (riga1 < n_righe - 1 && riga2 < n_righe - 1) {
                 *(testo_crittografato + i) = *(mat_sostituzione + riga1 + 1 * n_col + colonna1);
                 *(testo_crittografato + i + 1) = *(mat_sostituzione + riga2 + 1 * n_col + colonna2);
-            }
-            else if (riga1 == n_righe - 1)
-            {
+            } else if (riga1 == n_righe - 1) {
                 *(testo_crittografato + i) = *(mat_sostituzione + 0 * n_col + colonna1);
                 *(testo_crittografato + i + 1) = *(mat_sostituzione + riga2 + 1 * n_col + colonna2);
-            }
-            else
-            {
+            } else {
                 *(testo_crittografato + i) = *(mat_sostituzione + 0 * n_col + colonna1);
                 *(testo_crittografato + i + 1) = *(mat_sostituzione + riga2 + 1 * n_col + colonna2);
             }
 
             i = i + 2;
-        }
-        else
-        {
-
+        } else {
             *(testo_crittografato + i) = *(mat_sostituzione + riga1 * n_col + colonna2);
             *(testo_crittografato + i + 1) = *(mat_sostituzione + riga2 * n_col + colonna1);
             i = i + 2;
--- main.c
+++ main.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Funzioni.h"
-int main(int argc, char const *argv[])
-{
-char testo_crittografato[100];
-char messaggio[]="LET US MEET AT NOON";
-char mat_sostituzione[][8]={
-{'8','J','E','Q','D','N','5','O'},
-{'P','U','3','A','R','F','L','W'},
-{'4','V','C','2','T','M','B','I'},
-{'K','7','Z','S','G','X','H','Y'}};
-controllo_lettere(messaggio);
-// stampa_a2d_char(&mat_sostituzione[0][0],4,8);
-Crittografia(messaggio,&mat_sostituzione[0][0],4,8,testo_crittografato);
-printf("\n%s\n",messaggio);
-printf("\n%s\n",testo_crittografato);
 
+int main(int argc, char const *argv[]) {
+    char testo_crittografato[100] = "";
+    // char messaggio[100]="LET US MEET AT NOON";
+    char messaggio[100] = "";
+    char mat_sostituzione[][8] = {
+        {'8', 'J', 'E', 'Q', 'D', 'N', '5', 'O'},
+        {'P', 'U', '3', 'A', 'R', 'F', 'L', 'W'},
+        {'4', 'V', 'C', '2', 'T', 'M', 'B', 'I'},
+        {'K', '7', 'Z', 'S', 'G', 'X', 'H', 'Y'}
+    };
+
+    // stampa_a2d_char(&mat_sostituzione[0][0],4,8);
+
+    printf("Inserisci messaggio da crittografare:");
+    gets(messaggio);
+    printf("\n%s\n", messaggio);
+    Crittografia(messaggio, &mat_sostituzione[0][0], 4, 8, testo_crittografato);
+
+    printf("\n%s\n", testo_crittografato);
 }
